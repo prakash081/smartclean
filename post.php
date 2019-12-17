e80bec25b72ec623d83ed86e0347fb0658c1867d
prakash <swain.prakash0811@gmail.com>
2019-12-18 00:12:38 +0530

    code changed

--- post.php
+++ post.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Post - Start Bootstrap </title>
+    <title>Blog Post Start Bootstrap </title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
