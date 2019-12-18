65956758b34036085503a22b04e0a770766e2223
Robin <32897617+robin-padhi-07@users.noreply.github.com>
2019-12-18 22:50:54 +0530

    New Admin Theams Added

---
+++ new_admin/static/js/base.js
@@ -0,0 +1,9 @@
+// setuptools==41.6.0.post20191101
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
